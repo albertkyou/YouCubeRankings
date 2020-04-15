ca3cd3c1f141dbadb355629ffadb7d7bb862b2e8
albertkyou <albert.you@berkeley.edu>
2020-04-15 13:44:34 -0700

    update DB

--- dbfunctions.py
+++ dbfunctions.py
@@ -1,4 +1,4 @@
-def write_to_csv(channelName, user_id, score):
+def write_to_csv(user_id, score):
     import pandas as pd 
 
     df = pd.read_csv('db.csv')
@@ -6,7 +6,7 @@ def write_to_csv(channelName, user_id, score):
     
     uploadid = 'UU'+user_id[2:]
     
-    df.loc[df.index.max() + 1, :4] = [channelName, user_id, uploadid,score]
+    df.loc[df.index.max() + 1, 1:4] = [user_id, uploadid,score]
     df = df.sort_values(by='score',ascending=False)
     df.to_csv('db.csv',index=False)
 
--- get_stats.py
+++ get_stats.py
@@ -2,13 +2,12 @@ import calc_score as csp
 import dbfunctions as dbfn 
 import get_channel_id as gci 
 
-def get_stats(channelName):
-    upload_id, user_id = gci.get_channel_id(channelName)
+def get_stats(user_id):
     video_urls = csp.fetch_video_urls(user_id)
     score = csp.calc_score(video_urls)
 
-    dbfn.write_to_csv(channelName,user_id, score)
+    dbfn.write_to_csv(user_id, score)
     print('Done')
     return None
 
-get_stats('MeMyselfAndPi')
+get_stats('UCqTVfT9JQqhA6_Hi_h_h97Q')
