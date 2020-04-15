13fd0ff0a3222910b638db727e06a40d1772d3df
albertkyou <albert.you@berkeley.edu>
2020-04-15 10:58:29 -0700

    added database

--- calc_score.py
+++ calc_score.py
@@ -78,5 +78,20 @@ def calc_score(video_urls):
     print(score)
     return score
 
-video_urls = fetch_video_urls(user_id)
-calc_score(video_urls)
+def write_to_csv(user_id, score):
+    import pandas as pd 
+
+    df = pd.read_csv('db.csv')
+
+    
+    uploadid = 'UU'+user_id[2:]
+    
+    df.loc[df.index.max() + 1, :] = [user_id, uploadid,score]
+
+    df.to_csv('db.csv',index=False)
+
+
+
+
+# video_urls = fetch_video_urls(user_id)
+write_to_csv(user_id,10000)
